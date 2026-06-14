afaf67b76bfd87d0631a17dd29852385923e3764
KamiSama0110 <kamisama011004@gmail.com>
2026-06-14 17:33:25 -0400

    mejroras del toast en vista movil

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <RouterView />
-  <Toast position="top-right" />
+  <Toast :position="toastPosition" class="app-toast" />
 </template>
 
 <script setup>
+import { computed, onBeforeUnmount, onMounted, ref } from 'vue'
 import { RouterView } from 'vue-router'
 import Toast from 'primevue/toast'
+
+const isMobile = ref(false)
+
+function updateViewportState() {
+  isMobile.value = window.matchMedia('(max-width: 640px)').matches
+}
+
+const toastPosition = computed(() => (isMobile.value ? 'bottom-center' : 'top-right'))
+
+onMounted(() => {
+  updateViewportState()
+  window.addEventListener('resize', updateViewportState, { passive: true })
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateViewportState)
+})
 </script>
